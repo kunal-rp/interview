a39725a53da604f9db34a18b6ba4927a4942f5d0
kunal-rp <kunal@KRP-Silver.local>
2024-03-08 15:45:18 -0800

    api to save and retrieve bounding box dimensions

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const expressFileUpload = require("express-fileupload");
+var bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
 app.use(expressFileUpload());
+app.use(bodyParser.json());
 app.use(
 	cors({
 		methods: ["POST", "GET"],
@@ -27,6 +29,37 @@ var getImage = (req, res) => {
 
 app.post("/upload", uploadRoute);
 app.get("/getImage", getImage);
+
+var dimensions = [
+	{
+		id: parseInt(Date.now().toString().slice(-6)),
+		x: 50,
+		y: 30,
+		xLen: 110,
+		yLen: 100,
+		color: "#FFFFFF",
+	},
+	{
+		id: parseInt((Date.now() + 10).toString().slice(-6)),
+		x: 170,
+		y: 65,
+		xLen: 100,
+		yLen: 80,
+		color: "#FFFFFF",
+	},
+];
+
+var updateDimensions = (req, res) => {
+	dimensions = req.body;
+	res.json({});
+};
+
+var getDimensions = (req, res) => {
+	res.json({ dimensions: dimensions });
+};
+
+app.post("/updateDimensions", updateDimensions);
+app.get("/getDimensions", getDimensions);
 
 app.listen(port, () => {
 	console.log(`running on port ${port}`);
